069ed77493602cf5890846bf1bdd7e7e6ad4e1d6
luizfrz <luizfranca2005@gmail.com>
2026-06-12 13:58:29 -0300

    fix: theory

--- source/testing.py
+++ source/testing.py
@@ -1,21 +1,19 @@
 import random
 
-l1 = [
-    "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P","Q", "R","S", "T", "U", "V","W", "X", "Y", "Z"
-]
-l2 = [
+result = []
+letter = [
     "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N","O", "P","Q", "R","S", "T", "U", "V","W", "X", "Y", "Z"
 ]
 
 n = 0 
-# Random OI 
-print("Frase Sendo gerada...")
-while n < 10:
-        join = random.choice(l1)
-        join2 = random.choice(l1)
-        toBring = join + join2 
-        print(toBring)
-        if toBring == "OI":
+for _ in range(1000):
+    tentativas = 0
+
+    while True:
+        tentativas += 1
+
+        letter = random.choice(letter) + random.choice(letter)
+
+        if letter.lower() == "ola":
+            result.append(tentativas)
             break
-        
-        
